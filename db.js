39bc3956a83c2997ae070b42fca2e0e159de629b
Rehan <rehnx95@gmail.com>
2026-08-30 12:07:19 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,10 @@
-require("dotenv").config();
-const { Pool, types } = require("pg"); // 1. Added 'types' import
-
-// 2. Force Postgres TIMESTAMP (1114) and TIMESTAMPTZ (1184) to return as clean text strings
-types.setTypeParser(1114, (val) => val.replace("T", " ").split(".")[0]); 
-types.setTypeParser(1184, (val) => val.replace("T", " ").split(".")[0]);
-
 const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT) || 5432,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-module.exports = pool;
